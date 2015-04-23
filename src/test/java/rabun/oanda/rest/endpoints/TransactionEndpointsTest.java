933e301217c37523e8da831319918617a6a428bb
valeravorobjev <vorobjevvalera@gmail.com>
2015-04-23 19:49:11 +0300

    Added transactionEndpoints Test (don't complete)

---
+++ src/test/java/rabun/oanda/rest/endpoints/TransactionEndpointsTest.java
@@ -0,0 +1,25 @@
+package rabun.oanda.rest.endpoints;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransactionEndpointsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTransactions() throws Exception {
+
+    }
+}
